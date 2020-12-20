08303601ae6a24b7df1ee70f55ecebe68f1af026
Liz Giddings <lgiddings03@gmail.com>
2020-12-19 18:01:45 -0600

    creating first promt and conditions for password critera

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var length = Number(prompt ("How many characters would you like your password to be?"));
+while (isNaN(legth)|| lenght < 8 || lenght >128) lenght = Numbers (prompt "length must be between 8-128 characters")); 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
