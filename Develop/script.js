1f04221a0c9d96123f9230e210bb28e3ed518fa1
Liz Giddings <lgiddings03@gmail.com>
2020-12-19 22:57:41 -0600

    fished up some steps

--- Develop/script.js
+++ Develop/script.js
@@ -1,42 +1,40 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var length = Number(prompt("How many characters would you like your password to be?"));
-while (isNaN(length) || length < 8 || lenght > 128) length = Numbers(prompt "length must be between 8-128 characters"));
+var pass = "";
+function writePassword() {
+  var plength = prompt("How many characters would you like your password to be?");
+  if (plength < 8 || plength > 128) {
+    alert("Password length must be between 8-128!");
+    return;
+  }
+  if (plength >= 8 && plength <= 128) {
+    var uppers = confirm("Would you like uppercase letters?");
+    var lowers = confirm("Would you like lowercase letters?");
+    var numbers = confirm("Would you like numbers?");
+    var symbols = confirm("would you like special charactrers?");
+  }
+  if (uppers) password += (uppers = "ABCDEFGHIJKLMNOPQURSTUVWXYZ");
+  if (lowers) password += (lowers = "abcdefghijklmnopqrstuvwxyz");
+  if (numbers) password += (nummbers = "0123456789");
+  if (symbols) password += (symbols = "!@#$%^&*()-_=+[]{};:'<>/?'");
 
-var uppers = confirm("Would you like uppercase letters?");
-var lowers = confirm("Would you like lowercase letters?");
-var numbers = confirm("Would you like numbers?");
-var symbols = confirm("would you like special charactrers?");
-
-while (!uppers && !lowers && !numbers && !symbols) {
-  alert("you must select character type");
-  uppers = confirm("Would you like uppercase letters?");
-  lowers = confirm("Would you like lowercase letters?");
-  numbers = confirm("Would you like numbers?");
-  symbols = confirm("Would you like special characters?");
+  var password = generatePassword(pass);
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+  console.log(password);
 }
+function generatePassword(key) {
+  var generatedKey = "";
+  key, pass;
+  for (i = 0; i < pass.length; i++) {
+    generatedKey += key.charAt(Math.floor(Math.random() * pass.length));
+  }
+}
+var generateBtn = document.querySelector("#generate");
+
+
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-.passwordText.value = password;
 
-  if (uppers) password += randoSequence(allowed.uppers = "ABCDEFGHIJKLMNOPQURSTUVWXYZ");
-  if (lowers) password += randoSequence(allowed.lowers = "abcdefghijklmnopqrstuvwxyz");
-  if (numbers) password += randoSequence(allowed.nummbers = "0123456789");
-  if (symbols) password += randoSequence(allowed.symbols = "!@#$%^&*()-_=+[]{};:'<>/?'");
-
-  let(var i = password.lenght; i < length; i++) password += ((allowed).value);
-  document.getElementById("password").value = randoSequence("password").join("");
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-newFunction(); "generatePassword();" > Generate New Password</button >
-  function newFunction() {
-    <input type="text" id="password" />
-      ,
-      <button onclick />;
-  }
-
