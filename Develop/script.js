7cbd6d327cfc6e86e6a2a82463875fb6c9880836
Liz Giddings <lgiddings03@gmail.com>
2020-12-21 19:45:37 -0600

    took out unecessary if statements and added more variables.

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,28 @@
 // Assignment Code
-var pass = "";
 function writePassword() {
   var plength = prompt("How many characters would you like your password to be?");
   if (plength < 8 || plength > 128) {
     alert("Password length must be between 8-128!");
     return;
   }
-  if (plength >= 8 && plength <= 128) {
+  else {
     var uppers = confirm("Would you like uppercase letters?");
     var lowers = confirm("Would you like lowercase letters?");
     var numbers = confirm("Would you like numbers?");
     var symbols = confirm("would you like special charactrers?");
   }
-  if (uppers) password += (uppers = "ABCDEFGHIJKLMNOPQURSTUVWXYZ");
-  if (lowers) password += (lowers = "abcdefghijklmnopqrstuvwxyz");
-  if (numbers) password += (nummbers = "0123456789");
-  if (symbols) password += (symbols = "!@#$%^&*()-_=+[]{};:'<>/?'");
-
-  var password = generatePassword(pass);
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-  console.log(password);
 }
-function generatePassword(key) {
-  var generatedKey = "";
-  key, pass;
-  for (i = 0; i < pass.length; i++) {
-    generatedKey += key.charAt(Math.floor(Math.random() * pass.length));
+function generatePassword(plength,password) {
+  var plength = "";
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.plength;
+  for ( var i = 0; i < plength; i++ ) {
+     plength += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
+  return plength;
 }
 var generateBtn = document.querySelector("#generate");
 
